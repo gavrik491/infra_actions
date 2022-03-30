4ffcee0ded16d29800ea4c6c250522caf0a55ee4
gavrik491 <gavri49mgdn@mail.ru>
2022-03-30 13:25:51 +0300

    new workflow steps

--- infra_project/infra_project/urls.py
+++ infra_project/infra_project/urls.py
@@ -13,10 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django import urls
 from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
-    urls.path('', urls.include('infra_app.urls', namespace='infra_app')),
-    urls.path('admin/', admin.site.urls),
+    path('', include('infra_app.urls', namespace='infra_app')),
+    path('admin/', admin.site.urls),
 ]
